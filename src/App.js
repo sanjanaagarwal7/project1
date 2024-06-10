542d5848634b49b3903d54de729d32657f0b178f
sanjanaagarwal7 <sanjana96212@gmail.com>
2024-06-10 13:57:12 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-
+import React from 'react';
 import './App.css';
+
+import Header from "./Header";
+import Nav from "./Nav";
 
 function App() {
   return (
-    <div className="App">
-      Homepage hello
-      
-    </div>
+    <>
+    <Header/>
+    <Nav/>
+    </>
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+function Header(){
+    var url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_vsRr6NGdZxBWI2M8SVGdfpsEzvyir7tPog&s";
+    return(
+        <>
+        <img src={url}></img>
+        </>
+    )
+}
+
+export default Header;
